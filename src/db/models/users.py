f7b47ef776cbd4f3853696d9ca3618771cf8f31a
axdjuraev <axdjuraev@gmail.com>
2024-03-15 11:04:15 +0500

    add (db:models): users

---
+++ src/db/models/users.py
@@ -0,0 +1,10 @@
+import sqlalchemy as sa
+from .base import BaseModel
+
+
+class Users(BaseModel):
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    first_name = sa.Column(sa.String)
+    last_name = sa.Column(sa.String)
+    email = sa.Column(sa.String)
+    password_hash = sa.Column(sa.String)
